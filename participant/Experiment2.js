420165aa51170f0b58a7976912f3ac9b27cdf88f
kazuwo <623taiki@gmail.com>
2016-12-08 14:54:00 +0900

    add single swipeableViews

--- participant/Experiment2.js
+++ participant/Experiment2.js
@@ -4,10 +4,9 @@ import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
 import { Card, CardText } from 'material-ui/Card'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
-import Slider from 'material-ui/Slider'
-import Chip from 'material-ui/Chip'
 
 import { nextQuestion } from './actions'
+import Evaluation from './Evaluation'
 
 const mapStateToProps = ({ sequence, qswap, }) => ({
   sequence, qswap, 
@@ -21,62 +20,11 @@ class Experiment2 extends Component {
     }
   }
 
-   handleChange(event, value){
-    this.setState({ value: value })
-  }
-
-add(num){
-    this.setState({
-      value: this.state.value + num
-    })
-  }
-  
-
   render() {
     const { sequence, qswap, finEx2} = this.props
     return(
-      <Card>
-        <div style={{
-            display: "inline-block",
-            padding: "0%",
-            position: "relative",
-            left: (this.state.value*100/5) + "%",
-            transform: "translate(-50%, 0%)",
-            width: "auto",
-          }}>
-           <Chip
-            style={{
-              display: "inline-block"
-            }}
-            onClick={this.add.bind(this, -1)}
-          >{"<"}</Chip>
-          <Chip
-            style={{
-              display: "inline-block"
-            }}
-            >{this.state.value}</Chip>
-          <Chip
-            style={{
-              display: "inline-block"
-            }}
-            onClick={this.add.bind(this, 1)}
-          >{">"}</Chip>
-        </div>
-        <Slider
-          min={0}
-          max={5}
-          step={1}
-          value={this.state.value}
-          onChange={this.handleChange.bind(this)}
-        />
-        <RaisedButton
-          label='next'
-          primary='true'
-          onClick={finEx2.bind(this, "finEx2")}
-        />
-      </Card>
+      <Evaluation finEx2={finEx2}/>
     )
-
   }
 }
 
